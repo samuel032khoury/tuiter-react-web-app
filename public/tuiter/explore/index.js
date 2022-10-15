eaa176ca59e3733e6ed386a4b2ed0cf113a443c8
Samuel Ji <samuel.j032@icloud.com>
2022-10-15 18:18:44 -0400

    fix import

--- public/tuiter/explore/index.js
+++ public/tuiter/explore/index.js
@@ -1,6 +1,6 @@
-import NavigationSidebar from "../NavigationSidebar";
-import WhoToFollowList from "../WhoToFollowList";
-import PostSummaryList from "../PostSummaryList";
+import NavigationSidebar from "../NavigationSidebar/index.js";
+import WhoToFollowList from "../WhoToFollowList/index.js";
+import PostSummaryList from "../PostSummaryList/index.js";
 
 function exploreComponent() {
    $('#wd-explore').append(`
